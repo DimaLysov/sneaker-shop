faff3cc6ec8130fae7e7cca709c0ee0cea33fe92
Lysov Dmitrii <dmlysov@mail.ru>
2025-05-11 21:40:00 +0300

    fix

--- back/backend/settings.py
+++ back/backend/settings.py
@@ -103,6 +103,7 @@ DATABASES = {
         "PASSWORD": os.getenv('POSTGRES_PASSWORD'),
         "HOST": os.getenv('POSTGRES_HOST'),
         "PORT": os.getenv('POSTGRES_PORT'),
+        'CONN_MAX_AGE': 60,
     }
 }
 
